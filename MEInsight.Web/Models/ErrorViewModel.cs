9e118f0a3b10badbea58368a493eecd24ddb0dcf
mmahomar <mmahomar@edc.org>
2022-05-22 14:17:46 -0400

    Create .NET Core 6 projects
    Upgraded MEInsight.sln solution and projects to .NET Core 6
    Renamed MEL.Web to MEInsight.Web
    Renamed MEL.Entities to MEInsight.Entities
    Merged MEL.Data to MEInsight.Web.Data
    Added NuGet dependencies

---
+++ MEInsight.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MEInsight.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
