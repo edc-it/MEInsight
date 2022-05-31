899888cf5510a4ce8fad71684efe4bf6f4efc487
mmahomar <mmahomar@edc.org>
2022-05-31 01:38:05 -0400

    Add Excel import capability
    Added HelperExtensions, DataImports Controller, and View to start Excel import development
    #9

---
+++ MEInsight.Web/Controllers/DataImportsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MEInsight.Entities.Programs;
+using MEInsight.Web.Data;
+using MEInsight.Web.Extensions;
+
+namespace MEInsight.Web.Controllers
+{
+    public class DataImportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DataImportsController> _logger;
+        private IWebHostEnvironment _environment;
+
+        public DataImportsController(ApplicationDbContext context, ILogger<DataImportsController> logger, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _logger = logger;
+            _environment = environment;
+        }
+
+        // GET: DataImports
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Uploads an Excel formatted template related to the GroupId
+
+        /// </summary>
+        /// <param name="file">File to upload</param>
+        /// <param name="id">Group Id</param>
+        /// <returns></returns>
+        [HttpPost, ActionName("Upload")]
+        public IActionResult Upload(IFormFile file/*, Guid? id*/)
+        {
+            if (file == null)
+            {
+                TempData["messageType"] = "error";
+                TempData["messageTitle"] = "FILE NOT UPLOADED";
+                TempData["message"] = "A file could not be uploaded error";
+
+                return RedirectToAction("Index", "DataImports", new { /*id*/ });
+            }
+
+            // TODO
+            // Checks GroupId to import into
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //Group group = await _context.Groups.FindAsync(id);
+
+            //if (group == null)
+            //{
+            //    return NotFound();
+            //}
+
+            long size = file.Length;
+
+            if (size > 0)
+            {
+                // Limit file uploads to 50M
+                if (size > 52428800)
+                {
+                    TempData["messageType"] = "error";
+                    TempData["messageTitle"] = "FILE NOT UPLOADED";
+                    TempData["message"] = "File size exceeded the maximum size permitted (50MB)";
+                    return RedirectToAction("Index", "DataImports", new { /*id*/ });
+                }
+
+                //// Limit file types to .jpg, .jpeg, .png, .doc, and .pdf only
+                if (!CheckFileType(file.FileName))
+                {
+                    TempData["messageType"] = "error";
+                    TempData["messageTitle"] = "FILE NOT UPLOADED";
+                    TempData["message"] = "File type not valid. Upload Excel xlsx documents only";
+                    return RedirectToAction("Index", "DataImports", new { /*id*/});
+                }
+
+                // Read Excel file
+                var excelResult = HelperExtensions.ReadExcelSheet(file, true);
+
+                return View(excelResult);
+            }
+
+            TempData["messageType"] = "error";
+            TempData["messageTitle"] = "FILE NOT UPLOADED";
+            TempData["message"] = "A file could not be uploaded error";
+
+            return RedirectToAction("Index", "DataImports", new { /*id*/ });
+
+        }
+
+        #region Helpers
+        //Allowed file upload types
+        static bool CheckFileType(string fileName)
+        {
+            string ext = Path.GetExtension(fileName);
+            switch (ext.ToLower())
+            {
+                case ".xlsx":
+                    return true;
+                //case ".jpg":
+                //    return true;
+                //case ".jpeg":
+                //    return true;
+                //case ".png":
+                //    return true;
+                //case ".doc":
+                //    return true;
+                //case ".docx":
+                //    return true;
+                //case ".pdf":
+                //    return true;
+                default:
+                    return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ MEInsight.Web/Extensions/HelperExtensions.cs
@@ -0,0 +1,136 @@
+﻿using System.Data;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace MEInsight.Web.Extensions
+{
+    public static class HelperExtensions
+    {
+        public static DataTable ReadExcelSheet(IFormFile file, bool firstRowIsHeader = true)
+        {
+            List<string> Headers = new();
+            DataTable dt = new();
+            var readstream = file.OpenReadStream();
+
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(readstream, false))
+            {
+                //Read First WorkSheets
+                Sheet sheet = doc.WorkbookPart.Workbook.Sheets.GetFirstChild<Sheet>();
+                Worksheet worksheet = ((WorksheetPart)doc.WorkbookPart.GetPartById(sheet.Id.Value)).Worksheet;
+                IEnumerable<Row> rows = worksheet.GetFirstChild<SheetData>().Descendants<Row>();
+                
+                int counter = 0;
+                
+                foreach (Row row in rows)
+                {
+                    counter++;
+                    //Read the first row as header
+                    if (counter == 1)
+                    {
+                        var j = 1;
+                        foreach (Cell cell in row.Descendants<Cell>())
+                        {
+                            var columnName = firstRowIsHeader ? GetCellValue(doc, cell) : "Field" + j++;
+
+                            Headers.Add(columnName);
+                            dt.Columns.Add(columnName);
+                        }
+                    }
+                    else
+                    {
+                        dt.Rows.Add();
+                        int i = 0;
+                        foreach (Cell cell in row.Descendants<Cell>())
+                        {
+                            dt.Rows[^1][i] = GetCellValue(doc, cell);
+                            i++;
+                        }
+                    }
+                }
+
+            }
+            return dt;
+        }
+
+        public static void CreateExcelFile(DataTable table, string destination)
+        {
+            var ds = new DataSet();
+            ds.Tables.Add(table);
+            ExportDSToExcel(ds, destination);
+        }
+
+        private static string GetCellValue(SpreadsheetDocument doc, Cell cell)
+        {
+            string value = cell.CellValue.InnerText;
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return doc.WorkbookPart.SharedStringTablePart.SharedStringTable.ChildElements.GetItem(int.Parse(value)).InnerText;
+            }
+            return value;
+        }
+
+        public static void ExportDSToExcel(DataSet ds, string destination)
+        {
+            using var workbook = SpreadsheetDocument.Create(destination, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook);
+            var workbookPart = workbook.AddWorkbookPart();
+            workbook.WorkbookPart.Workbook = new Workbook
+            {
+                Sheets = new Sheets()
+            };
+
+            uint sheetId = 1;
+
+            foreach (DataTable table in ds.Tables)
+            {
+                var sheetPart = workbook.WorkbookPart.AddNewPart<WorksheetPart>();
+                var sheetData = new SheetData();
+                sheetPart.Worksheet = new Worksheet(sheetData);
+
+                Sheets sheets = workbook.WorkbookPart.Workbook.GetFirstChild<Sheets>();
+                string relationshipId = workbook.WorkbookPart.GetIdOfPart(sheetPart);
+
+                if (sheets.Elements<Sheet>().Any())
+                {
+                    sheetId =
+                        sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+                }
+
+                Sheet sheet = new() { Id = relationshipId, SheetId = sheetId, Name = table.TableName };
+                sheets.Append(sheet);
+
+                Row headerRow = new();
+
+                List<String> columns = new();
+                foreach (DataColumn column in table.Columns)
+                {
+                    columns.Add(column.ColumnName);
+
+                    Cell cell = new()
+                    {
+                        DataType = CellValues.String,
+                        CellValue = new CellValue(column.ColumnName)
+                    };
+                    headerRow.AppendChild(cell);
+                }
+
+                sheetData.AppendChild(headerRow);
+
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    Row newRow = new();
+                    foreach (String col in columns)
+                    {
+                        Cell cell = new()
+                        {
+                            DataType = CellValues.String,
+                            CellValue = new CellValue(dsrow[col].ToString())
+                        };
+                        newRow.AppendChild(cell);
+                    }
+
+                    sheetData.AppendChild(newRow);
+                }
+            }
+        }
+    }
+}
